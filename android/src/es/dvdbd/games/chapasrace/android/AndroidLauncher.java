ccce17925dc3142de4b1d4bed150ee79f2f9f404
dbuedo <davidbuedo@gmail.com>
2014-05-05 21:11:33 +0200

    Commit inicial

---
+++ android/src/es/dvdbd/games/chapasrace/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package es.dvdbd.games.chapasrace.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import es.dvdbd.games.chapasrace.ChapasGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChapasGame(), config);
+	}
+}
---
+++ desktop/src/es/dvdbd/games/chapasrace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.dvdbd.games.chapasrace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.dvdbd.games.chapasrace.ChapasGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChapasGame(), config);
+	}
+}
