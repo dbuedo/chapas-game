4fb57d1706f089db30e892d34cee9180c3b21314
dbuedo <davidbuedo@gmail.com>
2014-05-12 21:31:07 +0200

    Cambio de la clase de Android a la opcion correcta.

--- android/src/es/dvdbd/games/chapasrace/android/AndroidLauncher.java
+++ android/src/es/dvdbd/games/chapasrace/android/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import es.dvdbd.games.chapasrace.ChapasGame;
+
+import es.dvdbd.games.chapasrace.ChapasRaceGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ChapasGame(), config);
+		initialize(new ChapasRaceGame(), config);
 	}
 }
